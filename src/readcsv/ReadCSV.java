b2ec9931eba98408a2d2ac9641b939e1c7b17d42
Katerina <Katuwa-1995@mail.ru>
2016-12-22 12:00:04 +0200

    second commit

--- src/readcsv/ReadCSV.java
+++ src/readcsv/ReadCSV.java
@@ -7,17 +7,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * Created by Katerina on 21.12.2016 at 23:48.
- */
+ * @author Katerina
+ * @version 1.1
+*/
 public class ReadCSV {
+    /** * Класс для чтения информации с CSV файла
+     * считывает построчно, разбивая информацию по ячейкам
+     * разделитель ячейки - ";"
+     * @exception IOException - исключение, если файл не был создан
+     * @return  информацию в строках и столбцах таблицы
+     */
     public ArrayList csv() throws IOException {
         ArrayList<ArrayList<String>> allRowAndColData = null;
         ArrayList<String> oneRowData = null;
+        /** указание путя для выбора файла */
         String fName = "C:\\Users\\Katerina\\IdeaProjects\\JavaProject\\src\\readcsv\\inputInfo.csv";
         String currentLine;
+        /**открытие потока для работы с файлом */
         FileInputStream fis = new FileInputStream(fName);
         DataInputStream myInput = new DataInputStream(fis);
         int i = 0;
+        /** переносим данные  в массив */
         allRowAndColData = new ArrayList<ArrayList<String>>();
         while ((currentLine = myInput.readLine()) != null) {
             oneRowData = new ArrayList<String>();
--- src/readcsv/WriteDOCX.java
+++ src/readcsv/WriteDOCX.java
@@ -1,5 +1,4 @@
 package readcsv;
-
 import javafx.stage.FileChooser;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
@@ -9,50 +8,45 @@ import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRow;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTbl;
-
 import java.io.*;
 import java.util.ArrayList;
 
 /**
- * Created by Katerina on 21.12.2016 at 23:56.
+ * @author Katerina
+ * @version 1.0
+ *
  */
 public class WriteDOCX {
+    /** Класс для записи из шаблона в новый файл
+     * @param csvFile  - файл с таблицей, содержащей информацию
+     * @exception IOException - исключение, если файл не был создан
+     * @exception InvalidFormatException - исключение, если не верный формат файла
+     * */
     public void writeDoc(ArrayList csvFile) throws IOException, InvalidFormatException {
-        //XWPFDocument doc = new XWPFDocument();
         FileInputStream fis = new FileInputStream("C:\\Users\\Katerina\\IdeaProjects\\JavaProject\\src\\readcsv\\template.docx");
         XWPFDocument doc = new XWPFDocument(OPCPackage.open(fis));
 
-        /* create table */
+        /** создание новой таблицы */
         XWPFTable table = doc.getTables().get(0);
-        CTTbl ctTbl = CTTbl.Factory.newInstance(); // Create a new CTTbl for the new table
-
-        /* save doc file*/
+        CTTbl ctTbl = CTTbl.Factory.newInstance();
         FileChooser fileChooser = new FileChooser();
-
-        fileChooser.setTitle("Сохранить как ...");
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Документ Word (*.docx)", "*.docx");
-        fileChooser.getExtensionFilters().add(extFilter);
-        fileChooser.setInitialFileName("111.docx");
-        File userDirectory = new File("C:\\Users\\Katerina\\IdeaProjects\\JavaProject\\src\\readcsv\\111.doc");
+        /** указание пути к файлу*/
+        File userDirectory = new File("C:\\Users\\Katerina\\IdeaProjects\\JavaProject\\src\\readcsv\\result.doc");
         fileChooser.setInitialDirectory(userDirectory);
         int i = 0;
         try {
-
+            /** запись в таблицу*/
             for (i = 1; i < csvFile.size(); i++) {
                 ArrayList<?> ardata = (ArrayList<?>) csvFile.get(i);
                 XWPFTableRow row1 = table.createRow();
                 for (int k = 0; k < ardata.size(); k++) {
-                    //System.out.print(ardata.get(k));
                     XWPFTableCell cell = row1.createCell();
                     cell.setText(ardata.get(k).toString());
                 }
-              //  table.removeRow(i);
                 System.out.println();
-
             }
             System.out.println("Done");
-
-            /* write docx file */
+            /** запись doc файла */
             OutputStream outputStream = new FileOutputStream(new File(userDirectory.getAbsolutePath()));
             doc.write(outputStream);
             outputStream.flush();
